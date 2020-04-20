779d26837e406db8a2a83decf1f1d17d5b6a0b58
Carlos Giachetti <carlos_giachetti@hotmail.com>
2020-04-20 12:50:51 -0300

    Add files via upload

---
+++ App/App/Program.cs
@@ -0,0 +1,27 @@
+﻿using ACME.Interfaces;
+using System.Reflection;
+using static System.Configuration.ConfigurationManager;
+using static System.Console;
+using static System.Reflection.Assembly;
+
+
+namespace ACME
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string dllPath   = AppSettings["dllPath"];
+			string className = AppSettings["className"];
+
+			Assembly assembly = LoadFrom(dllPath);
+			IExample obj      = (IExample)assembly.CreateInstance(className);
+			string   output   = obj.Output();
+			
+			WriteLine(output);
+
+			WriteLine("\nPress any key...");
+			ReadLine();
+		}
+	}
+}
