63a31d53e0eb9fd2d44d405a2fc0606758454991
Carlos Giachetti <carlos_giachetti@hotmail.com>
2020-04-20 12:41:59 -0300

    Add files via upload

---
+++ Libraries/DLL_A/DLL_A/Example_A.cs
@@ -0,0 +1,17 @@
+﻿using ACME.Interfaces;
+
+
+namespace ACME.Libraries
+{
+	class Example_A : IExample
+	{
+		public Example_A()
+		{
+		}
+
+		public string Output()
+		{
+			return "ACME.Libraries.Example_A.Output()";
+		}
+	}
+}
---
+++ Libraries/DLL_B/DLL_B/Example_B.cs
@@ -0,0 +1,18 @@
+﻿using ACME.Interfaces;
+
+
+namespace ACME.Libraries
+{
+	class Example_B : IExample
+	{
+		public Example_B()
+		{
+		}
+
+		public string Output()
+		{
+			return "ACME.Libraries.Example_B.Output()";
+		}
+	}
+}
+
